5e472084500287c17f9578032f6323e085a2b238
Andrew Schwimmer <andrew.schwimmer@disneystreaming.com>
2021-04-27 22:44:51 -0400

    first commit

---
+++ src/main/java/schwimmer/openweathermap/OpenWeatherMapFeed.java
@@ -0,0 +1,19 @@
+package schwimmer.openweathermap;
+
+import java.util.Date;
+
+public class OpenWeatherMapFeed {
+
+    Main main;
+    String name;
+    long dt;
+
+    static class Main {
+        double temp;
+    }
+
+    public Date getTime() {
+        return new Date(dt * 1000);
+    }
+
+}
---
+++ src/main/java/schwimmer/openweathermap/OpenWeatherMapService.java
@@ -0,0 +1,22 @@
+package schwimmer.openweathermap;
+
+import io.reactivex.rxjava3.core.Single;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface OpenWeatherMapService {
+
+    @GET("/data/2.5/weather?appid=c65f21012c4876d2dc360667ec9a4a1b")
+    Single<OpenWeatherMapFeed> getCurrentWeather(
+            @Query("q") String location,
+            @Query("units") String units
+    );
+
+    @GET("/data/2.5/forecast?appid=c65f21012c4876d2dc360667ec9a4a1b")
+    Single<OpenWeatherMapForecast> getWeatherForecast(
+            @Query("q") String location,
+            @Query("units") String units
+    );
+
+
+}
